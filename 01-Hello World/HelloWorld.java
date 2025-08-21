602dbb6fffd8b2f7c6ac99a92ce1bd393604677b
MK-Alam <mksrizon.alam93@gmail.com>
2025-08-21 13:52:22 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -7,6 +7,9 @@ public class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World");
+        System.out.println("My Name: Mohammad Khairul Alam");
+        System.out.println("Course Name: Object Oriented Programming In Java I");
+        System.out.println("Course Code: CSC 200");
+        System.out.println("Lecturer Name: Sathish Sarma");
     }
 }
